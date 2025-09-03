e3c965aa5245ffcd3b47ad77fea20cef69af4d68
solly29 <cksdud1311@naver.com>
2025-09-04 00:58:45 +0900

    최초 프로젝트 세팅

---
+++ src/main/kotlin/com/solly/commonspring/CommonSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.solly.commonspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommonSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommonSpringApplication>(*args)
+}
