19fc870f060e3c854d742d1f74b50ba2f227549f
cksdud1311 <cksdud1311@naver.com>
2025-09-29 10:07:58 +0900

    - advice 자동 구성 적용

--- src/main/kotlin/com/solly/commonspring/autoConfig/GlobalExceptionHandlerAutoConfig.kt
+++ src/main/kotlin/com/solly/commonspring/autoConfig/GlobalExceptionHandlerAutoConfig.kt
@@ -1,24 +1,24 @@
-//package com.solly.commonspring.autoConfig
-//
-//import com.solly.commonspring.advice.GlobalExceptionHandler
-//import com.solly.commonspring.advice.JpaExceptionHandler
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//
-//@Configuration
-//class GlobalExceptionHandlerAutoConfig {
-//
-//    @Bean
-//    fun globalExceptionHandler() = GlobalExceptionHandler()
-//
-//}
-//
-//@Configuration
-//@ConditionalOnClass(name = ["javax.persistence.NoResultException"])
-//class JpaExceptionHandlerAutoConfig {
-//
-//    @Bean
-//        fun jpaExceptionHandler() = JpaExceptionHandler()
-//
-//}
+package com.solly.commonspring.autoConfig
+
+import com.solly.commonspring.advice.GlobalExceptionHandler
+import com.solly.commonspring.advice.JpaExceptionHandler
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class GlobalExceptionHandlerAutoConfig {
+
+    @Bean
+    fun globalExceptionHandler() = GlobalExceptionHandler()
+
+}
+
+@Configuration
+@ConditionalOnClass(name = ["javax.persistence.NoResultException"])
+class JpaExceptionHandlerAutoConfig {
+
+    @Bean
+    fun jpaExceptionHandler() = JpaExceptionHandler()
+
+}
